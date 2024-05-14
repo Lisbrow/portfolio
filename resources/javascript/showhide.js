abec5b6509f833d5c4d3536c94352d7e3e3c61bc
Lisbrow <l.brown91910@gmail.com>
2024-05-13 17:35:00 -0700

    Add media queries for splash section

--- resources/javascript/showhide.js
+++ resources/javascript/showhide.js
@@ -1,18 +1,19 @@
 // Function to show section by ID
 function showSection(sectionId) {
-    console.log('Showing section:', sectionId); // Debug statement
     // Hide all sections
     document.querySelectorAll('#section-container > section').forEach(section => {
-        section.style.opacity = 0; // Set opacity to 0 to hide
+        section.style.opacity = 0;
         section.style.transition = 'opacity 1s'; // Add transition for opacity
-        section.style.display = 'none'; // Set display to none
+        section.style.display = 'none';
     });
+
     // Show the selected section
     const selectedSection = document.getElementById(sectionId);
-    selectedSection.style.display = 'flex'; // Changed from 'block' to 'flex' for flexbox layout
+    selectedSection.style.display = 'flex';
     setTimeout(() => {
-        selectedSection.style.opacity = 1; // Set opacity to 1 to show
+        selectedSection.style.opacity = 1;
     }, 30); // Delay to ensure display change takes effect before fading in
+    
     // Show the header and footer
     showHeaderAndFooter(); // Show header and footer
 }
@@ -21,17 +22,17 @@ function showSection(sectionId) {
 function showHeaderAndFooter() {
     const header = document.querySelector('header');
     const footer = document.querySelector('footer');
-    header.style.display = 'flex'; // Set header to display as flex container
-    footer.style.display = 'flex'; // Set footer to display as flex container
+    header.style.display = 'flex';
+    footer.style.display = 'flex';
     setTimeout(() => {
-        header.style.opacity = 1; // Set opacity to 1 to show
-    }, 200);
+        header.style.opacity = 1;
+    }, 200); // Delay to ensure display change takes effect before fading in
     setTimeout(() => {
-        footer.style.opacity = 1; // Set opacity to 1 to show
-    }, 200);
+        footer.style.opacity = 1;
+    }, 200); // Delay to ensure display change takes effect before fading in
 }
 
-// Add event listeners to links
+// Add event listeners to header links
 document.getElementById('about-link').addEventListener('click', function (event) {
     event.preventDefault();
     showSection('about');
@@ -57,11 +58,12 @@ document.querySelectorAll('#splash-links a').forEach(link => {
         const sectionId = this.getAttribute('href').substring(1); // Get the section ID from the href attribute
         showSection(sectionId);
         showHeaderAndFooter(); // Call showHeaderAndFooter when a link in the splash section is clicked
+
         // Trigger exit animation for splash section
         const splashSection = document.getElementById('splash');
         splashSection.classList.add('exit');
         setTimeout(() => {
             splashSection.style.display = 'none'; // Hide the splash section after animation completes
-        }, 1000); // Adjust this timeout based on the animation duration
+        }, 1000);
     });
 });
